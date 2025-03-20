5c737544cee71a532414faf9083d88327e2dec24
Shafer <25ShaferR@mydacc.org>
2025-03-20 13:58:57 -0400

    Images + css

--- DBConn.php
+++ DBConn.php
@@ -6,6 +6,7 @@
 
         public function open() {
             $this->conn = new mysqli("localhost", "root", "password", "todo"); 
+            //$this->conn = new mysqli("10.4.52.48:3306", "lebron", "password", "todo");
         }
     
         public function close(){
